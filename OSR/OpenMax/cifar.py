f6f7fd694fb04da38c1a50398849a976d1b46945
13952522076 <xuma@my.unt.edu>
2020-09-13 18:19:21 -0500

    update

--- OSR/OpenMax/cifar.py
+++ OSR/OpenMax/cifar.py
@@ -104,7 +104,7 @@ def main():
         save_model(net, None, epoch, os.path.join(args.checkpoint,'last_model.pth'))
         test_loss, test_acc = None, None
         test(epoch, net, testloader, criterion,device)
-        logger.append([epoch+1, optimizer.param_groups[0]['lr'], train_loss, train_acc, train_acc, test_loss, test_acc])
+        logger.append([epoch+1, optimizer.param_groups[0]['lr'], train_loss, train_acc, test_loss, test_acc])
 
     logger.close()
 
