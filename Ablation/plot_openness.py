c21e0f58ff046e90d6566e3697923db74bbd5054
13952522076 <xuma@my.unt.edu>
2020-09-14 22:09:24 -0500

    update

--- Ablation/plot_openness.py
+++ Ablation/plot_openness.py
@@ -14,7 +14,7 @@ Known = np.arange(0.0, 100.0, 1.0)
 Unknown = np.arange(0.0, 100.0, 1.0)
 for i in range(len(Known)):
     for j in range(len(Unknown)):
-        print(f"{Known[i]}\t{Unknown[j]}\t{(Known[i])/(Unknown[j]+1e-7)}")
+        print(f"{Known[i]}\t{Unknown[j]}\t{(Known[i])/(Known[i]+Unknown[j]+1e-7)}")
 Known, Unknown = np.meshgrid(Known, Unknown)
 Openness = (Unknown/(Known+Unknown+1e-7))
 
