42b61d65fdb06bbe82166b31c051822dfe6c868a
13952522076 <xuma@my.unt.edu>
2020-09-16 19:00:06 -0500

    update stage1

--- OSR/OLTR/cifar100.py
+++ OSR/OLTR/cifar100.py
@@ -29,70 +29,64 @@ model_names = sorted(name for name in models.__dict__
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+
+# Dataset preperation
 parser.add_argument('--arch', default='ResNet18', choices=model_names, type=str, help='choosing network')
 parser.add_argument('--bs', default=256, type=int, help='batch size')
-parser.add_argument('--es', default=100, type=int, help='epoch size')
-parser.add_argument('--train_class_num', default=50, type=int, help='Classes used in training')
-parser.add_argument('--test_class_num', default=100, type=int, help='Classes used in testing')
-parser.add_argument('--includes_all_train_class', default=True,  action='store_true',
-                    help='If required all known classes included in testing')
-parser.add_argument('--evaluate', action='store_true',
-                    help='Evaluate without training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--evaluate', action='store_true', help='Evaluate without training')
 
-#Parameters for weibull distribution fitting.
-parser.add_argument('--weibull_tail', default=20, type=int, help='Classes used in testing')
-parser.add_argument('--weibull_alpha', default=3, type=int, help='Classes used in testing')
-parser.add_argument('--weibull_threshold', default=0.9, type=float, help='Classes used in testing')
+# Parameters for stage 1
+parser.add_argument('--stage1_resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('--stage1_es', default=30, type=int, help='epoch size')
+
 
 
 
 args = parser.parse_args()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+args.checkpoint = './checkpoints/cifar/' + args.arch
+if not os.path.isdir(args.checkpoint):
+    mkdir_p(args.checkpoint)
+
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = CIFAR100(root='../../data', train=True, download=True, transform=transform_train,
+                    train_class_num=args.train_class_num, test_class_num=args.test_class_num,
+                    includes_all_train_class=args.includes_all_train_class)
+
+testset = CIFAR100(root='../../data', train=False, download=True, transform=transform_test,
+                   train_class_num=args.train_class_num, test_class_num=args.test_class_num,
+                   includes_all_train_class=args.includes_all_train_class)
+
 
 
 
 def main():
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(device)
-    best_acc = 0  # best test accuracy
+    main_stage1()
+
+
+def main_stage1():
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-    # checkpoint
-    args.checkpoint = './checkpoints/cifar/' + args.arch
-    if not os.path.isdir(args.checkpoint):
-        mkdir_p(args.checkpoint)
-
-    # Data
-    print('==> Preparing data..')
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    trainset = CIFAR100(root='../../data', train=True, download=True, transform=transform_train,
-                        train_class_num=args.train_class_num, test_class_num=args.test_class_num,
-                        includes_all_train_class=args.includes_all_train_class)
+    # data loader
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.bs, shuffle=True, num_workers=4)
-    testset = CIFAR100(root='../../data', train=False, download=True, transform=transform_test,
-                       train_class_num=args.train_class_num, test_class_num=args.test_class_num,
-                       includes_all_train_class=args.includes_all_train_class)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.bs, shuffle=False, num_workers=4)
-
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=args.bs, shuffle=False, num_workers=4)
 
     # Model
     print('==> Building model..')
-    try:
-        net = models.__dict__[args.arch](num_classes=100) # CIFAR 100
-    except:
-        net = models.__dict__[args.arch]()
+    net = models.__dict__[args.arch](num_classes=args.train_class_num) # CIFAR 100
     net = net.to(device)
 
     if device == 'cuda':
@@ -108,29 +102,22 @@ def main():
             # best_acc = checkpoint['acc']
             # print("BEST_ACCURACY: "+str(best_acc))
             start_epoch = checkpoint['epoch']
-            logger = Logger(os.path.join(args.checkpoint, 'log.txt'), resume=True)
+            logger = Logger(os.path.join(args.checkpoint, 'log_stage1.txt'), resume=True)
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
     else:
-        logger = Logger(os.path.join(args.checkpoint, 'log.txt'))
-        logger.set_names(['Epoch', 'Learning Rate', 'Train Loss','Train Acc.', 'Test Loss', 'Test Acc.'])
+        logger = Logger(os.path.join(args.checkpoint, 'log_stage1.txt'))
+        logger.set_names(['Epoch', 'Learning Rate', 'Train Loss','Train Acc.'])
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
-    # test(0, net, trainloader, testloader, criterion, device)
-    epoch=0
-    if not args.evaluate:
-        for epoch in range(start_epoch, start_epoch + args.es):
-            print('\nEpoch: %d   Learning rate: %f' % (epoch+1, optimizer.param_groups[0]['lr']))
-            adjust_learning_rate(optimizer, epoch, args.lr)
-            train_loss, train_acc = train(net,trainloader,optimizer,criterion,device)
-            save_model(net, None, epoch, os.path.join(args.checkpoint,'last_model.pth'))
-            test_loss, test_acc = 0, 0
-            #
-            logger.append([epoch+1, optimizer.param_groups[0]['lr'], train_loss, train_acc, test_loss, test_acc])
-
-    test(epoch, net, trainloader, testloader, criterion, device)
+    for epoch in range(start_epoch, start_epoch + args.es):
+        print('\nStage_1 Epoch: %d   Learning rate: %f' % (epoch+1, optimizer.param_groups[0]['lr']))
+        adjust_learning_rate(optimizer, epoch, args.lr,step=10)
+        train_loss, train_acc = train(net,trainloader,optimizer,criterion,device)
+        save_model(net, None, epoch, os.path.join(args.checkpoint,'last_model.pth'))
+        logger.append([epoch+1, optimizer.param_groups[0]['lr'], train_loss, train_acc])
     logger.close()
 
 
