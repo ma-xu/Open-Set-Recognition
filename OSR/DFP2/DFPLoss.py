0edddc01a1bf934264326703731fa73407bda595
13952522076 <xuma@my.unt.edu>
2020-09-26 17:34:35 -0500

    update

--- OSR/DFP2/DFPLoss.py
+++ OSR/DFP2/DFPLoss.py
@@ -14,22 +14,21 @@ class DFPLoss(nn.Module):
             feat_dim (int): feature dimension.
         """
 
-    def __init__(self, num_classes=10, beta=1):
+    def __init__(self, beta=1):
         super(DFPLoss, self).__init__()
-        self.num_classes = num_classes
-        self.register_buffer("classes", torch.arange(self.num_classes).long())
         self.beta = beta
 
     def forward(self, dist, labels):
-        batch_size, _ = dist.shape
-        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
-        mask = labels.eq(self.classes.expand(batch_size, self.num_classes))
+        batch_size, num_classes = dist.shape
+        classes = torch.arange(num_classes, device=labels.device).long()
+        labels = labels.unsqueeze(1).expand(batch_size, num_classes)
+        mask = labels.eq(classes.expand(batch_size, num_classes))
         dist_within = (dist * mask.float()).sum(dim=1, keepdim=True)
 
         # dist_between = (dist * (1 - mask.float()))
         dist_between = F.relu(dist_within - dist, inplace=True)  # ensure within_distance greater others
         dist_between = dist_between.sum(dim=1, keepdim=False)
-        dist_between = dist_between / (self.num_classes - 1.0)
+        dist_between = dist_between / (num_classes - 1.0)
 
         loss_within = (dist_within.sum()) / batch_size
         loss_between = self.beta * (dist_between.sum()) / batch_size
@@ -45,7 +44,7 @@ class DFPLoss(nn.Module):
 
 def demo():
     x = torch.rand([3, 5])
-    label = torch.Tensor([0, 0, 2])
+    label = torch.Tensor([1, 3, 2])
     loss = DFPLoss(5)
     dist_loss = loss(x, label)
     print(dist_loss['total'])
