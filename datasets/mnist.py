d7a3f449d33ef45499f1da5d1d8aa2f1160acc54
13952522076 <xuma@my.unt.edu>
2020-09-26 20:17:23 -0500

    MNIST dataset

---
+++ datasets/mnist.py
@@ -0,0 +1,278 @@
+from __future__ import print_function
+from torchvision.datasets.vision import VisionDataset
+import warnings
+from PIL import Image
+import os
+import os.path
+import numpy as np
+import torch
+import codecs
+
+from torchvision.datasets.utils import download_url, download_and_extract_archive, extract_archive, \
+    makedir_exist_ok, verify_str_arg
+
+"""
+For making sense, the unkown classes starts from zero to number. 
+"""
+class MNIST(VisionDataset):
+    """`MNIST <http://yann.lecun.com/exdb/mnist/>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset where ``MNIST/processed/training.pt``
+            and  ``MNIST/processed/test.pt`` exist.
+        train (bool, optional): If True, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+    urls = [
+        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
+    ]
+    training_file = 'training.pt'
+    test_file = 'test.pt'
+    classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
+               '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine']
+
+    @property
+    def train_labels(self):
+        warnings.warn("train_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def test_labels(self):
+        warnings.warn("test_labels has been renamed targets")
+        return self.targets
+
+    @property
+    def train_data(self):
+        warnings.warn("train_data has been renamed data")
+        return self.data
+
+    @property
+    def test_data(self):
+        warnings.warn("test_data has been renamed data")
+        return self.data
+
+    def __init__(self, root, train=True, transform=None, target_transform=None,
+                 download=False, train_class_num=5, test_class_num=7, includes_all_train_class=True):
+        super(MNIST, self).__init__(root, transform=transform,
+                                    target_transform=target_transform)
+        self.train = train  # training set or test set
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
+
+        if self.train:
+            data_file = self.training_file
+        else:
+            data_file = self.test_file
+        self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
+        self._update_open_set(train_class_num, test_class_num, includes_all_train_class)
+
+    def _update_open_set(self, train_class_num=5, test_class_num=5, includes_all_train_class=False):
+        """
+        Author: Xu Ma (xuma@my.unt.edu)
+        Date: Sep/13/2020
+        :param train_class_num: known class number in training
+        :param test_class_num: original class number involoved in testing
+        :param includes_all_train_class: If contains all unknown classes in testing.
+        :return: Update the data, targets,classes and class_to_idx; add openness attribute.
+        """
+        assert train_class_num > 0 and train_class_num < 10  # not include 10 to ensure openness.
+        if includes_all_train_class:
+            assert test_class_num > train_class_num  # not include equal to ensure openness.
+        class_list = list(range(len(self.classes)))
+        train_classes = list(range(train_class_num))
+        if includes_all_train_class:
+            rnd = np.random.RandomState(42)
+            unknown_list = list(range(train_class_num,10))
+            test_classes = rnd.choice(unknown_list, test_class_num-train_class_num, replace=False).tolist()
+            test_classes = train_classes + test_classes
+        else:
+            rnd = np.random.RandomState(42)
+            test_classes = rnd.choice(class_list, test_class_num, replace=False).tolist()
+
+        # Update self.classes
+        selected_elements = [self.classes[index] for index in train_classes]
+        selected_elements.append(str(train_class_num)+'-unknown')
+        self.classes = selected_elements
+        # update self.class_to_idx
+        # Ignore: due to the class_to_idx property
+        # self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
+        # Processing data
+        if self.train:
+            indexes = [i for i, x in enumerate(self.targets) if x in train_classes]
+            self.data = self.data[indexes, :, :]
+            self.targets =[train_classes.index(self.targets[i]) for i in indexes]
+            print(f"\tTraining data includes {train_class_num} classes, {len(self.targets)} samples.")
+        else:
+            indexes = [i for i, x in enumerate(self.targets) if x in test_classes]
+            self.data = self.data[indexes, :, :]
+            temp_test_classes = [x for x in test_classes if x not in train_classes]
+            train_and_test_calsses = train_classes+temp_test_classes
+            self.targets = [train_and_test_calsses.index(self.targets[i]) for i in indexes]
+            for i in range(0,len(self.targets)) :
+                if self.targets[i]>train_class_num:
+                    self.targets[i] = train_class_num
+            print(f"\tTesting data includes {train_class_num+1} classes (Original {test_class_num} classes),"
+                  f" {len(self.targets)} samples.")
+            self.openness= float(len(temp_test_classes))/float(train_class_num+len(temp_test_classes))
+            print(f"\tDuring testing, openness is {self.openness}.")
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], int(self.targets[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode='L')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+    @property
+    def raw_folder(self):
+        return os.path.join(self.root, self.__class__.__name__, 'raw')
+
+    @property
+    def processed_folder(self):
+        return os.path.join(self.root, self.__class__.__name__, 'processed')
+
+    @property
+    def class_to_idx(self):
+        return {_class: i for i, _class in enumerate(self.classes)}
+
+    def _check_exists(self):
+        return (os.path.exists(os.path.join(self.processed_folder,
+                                            self.training_file)) and
+                os.path.exists(os.path.join(self.processed_folder,
+                                            self.test_file)))
+
+    def download(self):
+        """Download the MNIST data if it doesn't exist in processed_folder already."""
+
+        if self._check_exists():
+            return
+
+        makedir_exist_ok(self.raw_folder)
+        makedir_exist_ok(self.processed_folder)
+
+        # download files
+        for url in self.urls:
+            filename = url.rpartition('/')[2]
+            download_and_extract_archive(url, download_root=self.raw_folder, filename=filename)
+
+        # process and save as torch files
+        print('Processing...')
+
+        training_set = (
+            read_image_file(os.path.join(self.raw_folder, 'train-images-idx3-ubyte')),
+            read_label_file(os.path.join(self.raw_folder, 'train-labels-idx1-ubyte'))
+        )
+        test_set = (
+            read_image_file(os.path.join(self.raw_folder, 't10k-images-idx3-ubyte')),
+            read_label_file(os.path.join(self.raw_folder, 't10k-labels-idx1-ubyte'))
+        )
+        with open(os.path.join(self.processed_folder, self.training_file), 'wb') as f:
+            torch.save(training_set, f)
+        with open(os.path.join(self.processed_folder, self.test_file), 'wb') as f:
+            torch.save(test_set, f)
+
+        print('Done!')
+
+    def extra_repr(self):
+        return "Split: {}".format("Train" if self.train is True else "Test")
+
+
+def get_int(b):
+    return int(codecs.encode(b, 'hex'), 16)
+
+
+def open_maybe_compressed_file(path):
+    """Return a file object that possibly decompresses 'path' on the fly.
+       Decompression occurs when argument `path` is a string and ends with '.gz' or '.xz'.
+    """
+    if not isinstance(path, torch._six.string_classes):
+        return path
+    if path.endswith('.gz'):
+        import gzip
+        return gzip.open(path, 'rb')
+    if path.endswith('.xz'):
+        import lzma
+        return lzma.open(path, 'rb')
+    return open(path, 'rb')
+
+
+def read_sn3_pascalvincent_tensor(path, strict=True):
+    """Read a SN3 file in "Pascal Vincent" format (Lush file 'libidx/idx-io.lsh').
+       Argument may be a filename, compressed filename, or file object.
+    """
+    # typemap
+    if not hasattr(read_sn3_pascalvincent_tensor, 'typemap'):
+        read_sn3_pascalvincent_tensor.typemap = {
+            8: (torch.uint8, np.uint8, np.uint8),
+            9: (torch.int8, np.int8, np.int8),
+            11: (torch.int16, np.dtype('>i2'), 'i2'),
+            12: (torch.int32, np.dtype('>i4'), 'i4'),
+            13: (torch.float32, np.dtype('>f4'), 'f4'),
+            14: (torch.float64, np.dtype('>f8'), 'f8')}
+    # read
+    with open_maybe_compressed_file(path) as f:
+        data = f.read()
+    # parse
+    magic = get_int(data[0:4])
+    nd = magic % 256
+    ty = magic // 256
+    assert nd >= 1 and nd <= 3
+    assert ty >= 8 and ty <= 14
+    m = read_sn3_pascalvincent_tensor.typemap[ty]
+    s = [get_int(data[4 * (i + 1): 4 * (i + 2)]) for i in range(nd)]
+    parsed = np.frombuffer(data, dtype=m[1], offset=(4 * (nd + 1)))
+    assert parsed.shape[0] == np.prod(s) or not strict
+    return torch.from_numpy(parsed.astype(m[2], copy=False)).view(*s)
+
+
+def read_label_file(path):
+    with open(path, 'rb') as f:
+        x = read_sn3_pascalvincent_tensor(f, strict=False)
+    assert(x.dtype == torch.uint8)
+    assert(x.ndimension() == 1)
+    return x.long()
+
+
+def read_image_file(path):
+    with open(path, 'rb') as f:
+        x = read_sn3_pascalvincent_tensor(f, strict=False)
+    assert(x.dtype == torch.uint8)
+    assert(x.ndimension() == 3)
+    return x
+
+
+if __name__ == '__main__':
+    trainset = MNIST(root='./data', train=False, download=True)
