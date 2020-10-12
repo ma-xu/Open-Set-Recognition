46a490bc38c43d30d7435cf5819e9a3f4bde2bf1
13952522076 <xuma@my.unt.edu>
2020-10-12 14:29:30 -0500

    v2

---
+++ OSR/DFP_v2/DFPNet.py
@@ -0,0 +1,137 @@
+"""
+Version2: includes centroids into model, and shares embedding layers.
+"""
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import backbones.cifar as models
+from Distance import Distance
+
+
+class DFPNet(nn.Module):
+    def __init__(self, backbone='ResNet18', num_classes=1000,
+                 backbone_fc=False, embed_dim=None, classify_dim=512,
+                 distance='cosine', scaled=True, cosine_weight=1.0, thresholds=None):
+        """
+
+        :param backbone: the backbone architecture, default ResNet18
+        :param num_classes: known classes
+        :param backbone_fc: includes FC layers in backbone, default false.
+        :param include_dist: if include the distance branch, default false to get traditional backbone architecture.
+        :param embed_dim: if include the embedding layer and tell the embedding dimension.
+        :param embed_reduction: for embedding reduction in SENet style. May deprecated.
+        """
+        super(DFPNet, self).__init__()
+        assert backbone_fc == False  # drop out the classifier layer.
+        # num_classes = num_classes would be useless if backbone_fc=False
+        self.num_classes = num_classes
+        self.classify_dim = classify_dim
+        self.backbone_name = backbone
+        self.backbone = models.__dict__[backbone](num_classes=num_classes, backbone_fc=backbone_fc)
+        self.feat_dim = self.get_backbone_last_layer_out_channel()  # get the channel number of backbone output
+        if embed_dim:
+            self.embeddingLayer = nn.Sequential(
+                nn.ReLU(inplace=True),
+                nn.Linear(self.feat_dim, embed_dim)
+            )
+            self.feat_dim = embed_dim
+        # self.classifier = nn.Linear(self.feat_dim, num_classes)
+        # We add 1 centroid for the unknown class, which is like a placeholder.
+        self.centroids = nn.Parameter(torch.randn(num_classes + 1, self.classify_dim))
+        self.classifier = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Linear(self.feat_dim, self.classify_dim)
+        )
+        # self.init_parameters()
+
+        self.distance = distance
+        assert self.distance in ['l1', 'l2', 'cosine']
+        self.scaled = scaled
+        self.cosine_weight = cosine_weight
+        self.register_buffer("thresholds", thresholds)
+
+    def init_parameters(self):
+        # centroids = self.centroids-self.centroids.mean(dim=0,keepdim=True)
+        # # the greater std is, seems can achieve by
+        # centroids = centroids/(0.5*centroids.std(dim=0,keepdim=True))
+        #
+        # self.centroids = nn.Parameter(centroids)
+        nn.init.normal_(self.centroids,mean=0., std=2.)
+        # print(f"Initilized Centroids: \n {self.centroids}")
+        # print(f"Initilized Centroids STD: \n {torch.std(self.centroids,dim=0)}")
+        # print(f"Initilized Centroids MEAN: \n {torch.mean(self.centroids, dim=0)}")
+        # nn.init.normal_(self.centroids)
+
+
+    def get_backbone_last_layer_out_channel(self):
+        if self.backbone_name == "LeNetPlus":
+            return 128 * 3 * 3
+        last_layer = list(self.backbone.children())[-1]
+        while (not isinstance(last_layer, nn.Conv2d)) and \
+                (not isinstance(last_layer, nn.Linear)) and \
+                (not isinstance(last_layer, nn.BatchNorm2d)):
+
+            temp_layer = list(last_layer.children())[-1]
+            if isinstance(temp_layer, nn.Sequential) and len(list(temp_layer.children())) == 0:
+                temp_layer = list(last_layer.children())[-2]
+            last_layer = temp_layer
+        if isinstance(last_layer, nn.BatchNorm2d):
+            return last_layer.num_features
+        else:
+            return last_layer.out_channels
+
+    def generat_rand_feature(self, gap, sampler=6):
+        # generate a tensor with same shape as gap.
+        # n, c = gap.shape
+        # pool = gap.repeat(sampler, 1)  # repeat examples 3 times [n*sampler, c]
+        # pool_random = pool[torch.randperm(pool.size()[0])]
+        # pool_random = pool_random.view(sampler, n, c)
+        # pool_random = pool_random.mean(dim=0, keepdim=False)
+
+        pool_random = torch.rand(gap.size(), device=gap.device)
+        return pool_random
+
+    def forward(self, x):
+        # TODO: extract more outputs from the backbone like FPN, but for intermediate weak-supervision.
+        x = self.backbone(x)
+        dist_gen2cen = None
+        gap = (F.adaptive_avg_pool2d(x, 1)).view(x.size(0), -1)
+        # if includes embedding layer.
+        embed_fea = self.embeddingLayer(gap) if hasattr(self, 'embeddingLayer') else gap
+        classify_fea = self.classifier(embed_fea)
+
+        # calculate distance.
+        DIST = Distance(scaled=self.scaled, cosine_weight=self.cosine_weight)
+        normalized_centroids = F.normalize(self.centroids, dim=1, p=2)
+        dist_fea2cen = getattr(DIST, self.distance)(classify_fea, normalized_centroids)  # [n,c+1]
+        dist_cen2cen = DIST.l2(normalized_centroids, normalized_centroids)  # [c+1,c+1]
+
+
+        return {
+            "backbone_fea": x,
+            "classify_fea": classify_fea,
+            "embed_fea": embed_fea,
+            "dist_fea2cen": dist_fea2cen,
+            "dist_cen2cen": dist_cen2cen,
+            "dist_gen2cen": dist_gen2cen
+        }
+
+
+def demo():
+    x = torch.rand([10, 3, 32, 32])
+    net = DFPNet('ResNet18', num_classes=10, embed_dim=64, thresholds=torch.rand(11))
+    output = net(x)
+    # print(output["logits"].shape)
+    print(output["embed_fea"].shape)
+    print(output["dist_fea2cen"].shape)
+    print(output["dist_cen2cen"].shape)
+
+    # x = torch.rand([1, 1, 28, 28])
+    # net = DFPNet('LeNetPlus', num_classes=10, embed_dim=64)
+    # output = net(x)
+    # print(output["logits"].shape)
+    # print(output["embed_fea"].shape)
+    # print(output["dist_fea2cen"].shape)
+
+demo()
---
+++ OSR/DFP_v2/MyPlotter.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def plot_feature(net, plotloader, device,dirname, epoch=0,plot_class_num=10, maximum=500, plot_quality=150):
+    plot_features = []
+    plot_labels = []
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(plotloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            out = net(inputs)
+            embed_fea = out["embed_fea"]
+            try:
+                embed_fea = embed_fea.data.cpu().numpy()
+                targets = targets.data.cpu().numpy()
+            except:
+                embed_fea = embed_fea.data.numpy()
+                targets = targets.data.numpy()
+
+            plot_features.append(embed_fea)
+            plot_labels.append(targets)
+
+    plot_features = np.concatenate(plot_features, 0)
+    plot_labels = np.concatenate(plot_labels, 0)
+
+    # net_dict = net.state_dict()
+    # centroids = net_dict['module.centroids'] if isinstance(net, nn.DataParallel) \
+    #     else net_dict['centroids']
+
+    # try:
+    #     centroids = centroids.data.cpu().numpy()
+    # except:
+    #     centroids = centroids.data.numpy()
+    # print(centroids)
+    colors = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
+    for label_idx in range(plot_class_num):
+        features = plot_features[plot_labels == label_idx,:]
+        maximum = min(maximum, len(features)) if maximum>0 else len(features)
+        plt.scatter(
+            features[0:maximum, 0],
+            features[0:maximum, 1],
+            c=colors[label_idx],
+            s=1,
+        )
+    # plt.scatter(
+    #     centroids[:, 0],
+    #     centroids[:, 1],
+    #     # c=colors[label_idx],
+    #     c='black',
+    #     marker="*",
+    #     s=5,
+    # )
+    # currently only support 10 classes, for a good visualization.
+    # change plot_class_num would lead to problems.
+    legends= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    plt.legend(legends[0:plot_class_num]+['c'], loc='upper right')
+
+    save_name = os.path.join(dirname, 'epoch_' + str(epoch) + '.png')
+    plt.savefig(save_name, bbox_inches='tight',dpi=plot_quality)
+    plt.close()
+
+
+def plot_distance(net,
+                  plotloader: torch.utils.data.DataLoader,
+                  device: str,
+                  args
+                  ) -> dict:
+    print("===> Calculating distances...")
+    results = {i: {"distances": []} for i in range(args.train_class_num)}
+    threshold_list=[]
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(plotloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            out = net(inputs)
+            dist_fea2cen = out["dist_fea2cen"]  # [n, class_num]
+            dist_fea2cen = dist_fea2cen / args.cosine_weight  # rescale to [0,1].
+
+            for i in range(dist_fea2cen.shape[0]):
+                label = targets[i]
+                dist = dist_fea2cen[i, label]
+                results[label.item()]["distances"].append(dist)
+
+    for i in range(args.train_class_num):
+        # print(f"The examples number in class {i} is {len(results[i]['distances'])}")
+        cls_dist = results[i]['distances']  # distance list for each class
+        cls_dist.sort()  # python sort function do not return anything.
+        results[i]['distances'] = cls_dist
+        cls_dist = cls_dist[:-(args.tail_number)]  #remove the tail examples.
+
+        index = int(len(cls_dist) * (1 - args.p_value))
+        threshold = cls_dist[index].item()
+        threshold_list.append(threshold)
+        # cls_dist = cls_dist / (max(cls_dist))  # normalized to 0-1, we consider min as 0.
+        # min_distance = min(cls_dist)
+        min_distance = min(cls_dist)
+        max_distance = max(cls_dist)
+        hist = torch.histc(torch.Tensor(cls_dist), bins=args.bins, min=min_distance, max=max_distance)
+        results[i]['hist']=hist
+        results[i]['max'] = max_distance
+        results[i]['min'] = min_distance
+        results[i]['threshold'] = threshold
+    unknown_threshold = threshold-threshold-100. # we set threshold for unknown to -100. (actually 0 is fine)
+    threshold_list.append(unknown_threshold)
+    results['thresholds'] = torch.Tensor(threshold_list) # the threshold for unknown is 0.
+    torch.save(results,os.path.join(args.checkpoint, 'distance.pkl'))
+    print("===> Distance saved.")
+    return results
