afa1e213a99294b49fca9baf119fcae74bd84134
13952522076 <xuma@my.unt.edu>
2020-10-16 15:08:16 -0500

    reduce number of generated data

--- OSR/DFP_v5/Generater.py
+++ OSR/DFP_v5/Generater.py
@@ -1,12 +1,13 @@
 import torch
 
-def generater(inputs, targets,num_class,repeats=3):
+
+def generater_input(inputs, targets,num_class,repeats=3, reduce=8):
     b,c,w,h = inputs.shape
     g_data = inputs.repeat(repeats, 1,1,1)  # repeat examples 3 times [n*sampler, c]
     g_data = g_data[torch.randperm(g_data.size()[0])]
-    g_data = g_data.view(b,repeats,c,w,h)
-    g_data = g_data.mean(dim=1, keepdim=False)
-    g_label = num_class*torch.ones(b,dtype=targets.dtype)
+    g_data = g_data.view(-1, b//reduce,c,w,h)
+    g_data = g_data.mean(dim=0, keepdim=False)
+    g_label = num_class*torch.ones(b//reduce,dtype=targets.dtype)
     inputs= torch.cat([inputs,g_data],dim=0)
     targets = torch.cat([targets, g_label], dim=0)
 
@@ -22,10 +23,10 @@ def generater(inputs, targets,num_class,repeats=3):
 
 
 def demo():
-    n = 2
+    n = 16
     c = 5
     inputs = torch.rand([n, 1,3,3])
     targets = torch.empty(n, dtype=torch.long).random_(c)
-    generater(inputs, targets,n)
+    generater_input(inputs, targets,n)
 
 # demo()
--- OSR/DFP_v5/mnist.py
+++ OSR/DFP_v5/mnist.py
@@ -22,7 +22,7 @@ from Utils import adjust_learning_rate, progress_bar, Logger, mkdir_p, Evaluatio
 from DFPLoss import DFPLoss
 from DFPNet import DFPNet
 from MyPlotter import plot_feature, plot_distance
-from Generater import generater
+from Generater import generater_input
 
 model_names = sorted(name for name in models.__dict__
                      if not name.startswith("__")
@@ -196,7 +196,7 @@ def stage1_train(net, trainloader, optimizer, criterion, device):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets =generater(inputs, targets,args.train_class_num)
+        inputs, targets =generater_input(inputs, targets,args.train_class_num)
         inputs, targets = inputs.to(device), targets.to(device)
 
         optimizer.zero_grad()
