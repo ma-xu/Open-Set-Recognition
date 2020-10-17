bd23eff999ef3f8b9731c633b8a94a984c11eaf5
13952522076 <xuma@my.unt.edu>
2020-10-16 21:43:06 -0500

    update

--- OSR/DFP_v5/mnist.py
+++ OSR/DFP_v5/mnist.py
@@ -82,7 +82,7 @@ parser.add_argument('--tail_number', default=50, type=int,
 args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 args.checkpoint = './checkpoints/mnist/' + args.arch +\
-                  '/A%s_B%s_embed%s' % (args.alpha, args.beta,args.embed_dim)
+                  '/A%s_B%s_embed%s_%s' % (args.alpha, args.beta,args.embed_dim, args.distance)
 if not os.path.isdir(args.checkpoint):
     mkdir_p(args.checkpoint)
 
