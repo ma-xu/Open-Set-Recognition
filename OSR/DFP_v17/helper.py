64c19b6cd698b60c15c41f0103fe15d564fe31f0
13952522076 <xuma@my.unt.edu>
2020-11-11 16:00:28 -0600

    init v17

---
+++ OSR/DFP_v17/helper.py
@@ -0,0 +1,32 @@
+import torch
+from Utils import progress_bar
+
+def get_gap_stat(net, trainloader, device, args):
+
+    Features = {i: [] for i in range(args.train_class_num)}
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            out = net(inputs)
+            out = (out['gap']).unsqueeze(dim=1)
+            for i in range(out.shape[0]):
+                target = (targets[i]).item()
+                (Features[target]).append(out[i])
+            progress_bar(batch_idx, len(trainloader), "calculating statistics ...")
+
+        list_std = []
+        list_mean = []
+        for i in range(args.train_class_num):
+            feature = torch.cat(Features[i], dim=0)
+            list_std.append(torch.std(feature,dim=0,keepdim=True))
+            list_mean.append(torch.mean(feature, dim=0, keepdim=True))
+    list_std = torch.cat(list_std,dim=0)
+    list_mean = torch.cat(list_mean, dim=0)
+    return {"std":list_std,"mean":list_mean}
+
+
+
+
+
+
+
