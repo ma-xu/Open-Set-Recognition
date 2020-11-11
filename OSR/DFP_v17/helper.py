a35b61dcd6d1fcdb2060882cc775d785dec635f4
13952522076 <xuma@my.unt.edu>
2020-11-11 16:13:59 -0600

    feature bank

--- OSR/DFP_v17/helper.py
+++ OSR/DFP_v17/helper.py
@@ -3,7 +3,7 @@ from Utils import progress_bar
 
 def get_gap_stat(net, trainloader, device, args):
 
-    Features = {i: [] for i in range(args.train_class_num)}
+    Result = {i: {'fea_bank': []} for i in range(args.train_class_num)}
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(trainloader):
             inputs, targets = inputs.to(device), targets.to(device)
@@ -11,13 +11,22 @@ def get_gap_stat(net, trainloader, device, args):
             out = (out['gap']).unsqueeze(dim=1)
             for i in range(out.shape[0]):
                 target = (targets[i]).item()
-                (Features[target]).append(out[i])
+                (Result[target]["fea_bank"]).append(out[i])
+
+            # start check bank storage
+            # we believe 1024 samples in each class is representive enough.
+            for i in range(args.train_class_num):
+                if len(Result[i]["fea_bank"])>1024:
+                    Result[i]["fea_bank"] = Result[i]["fea_bank"][:1024]
+            # end check bank storage
+
             progress_bar(batch_idx, len(trainloader), "calculating statistics ...")
 
         list_std = []
         list_mean = []
         for i in range(args.train_class_num):
-            feature = torch.cat(Features[i], dim=0)
+            feature = torch.cat(Result[i]["fea_bank"], dim=0)
+            print(f"shape of Result[i][fea_bank] is {Result[i]['fea_bank'].shape}")
             list_std.append(torch.std(feature,dim=0,keepdim=True))
             list_mean.append(torch.mean(feature, dim=0, keepdim=True))
     list_std = torch.cat(list_std,dim=0)
