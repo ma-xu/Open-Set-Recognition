00df4b5cbc4f82847b71df19d1ce775d34b349e4
13952522076 <xuma@my.unt.edu>
2020-11-11 19:52:18 -0600

    update

--- OSR/DFP_v17/Generater.py
+++ OSR/DFP_v17/Generater.py
@@ -37,7 +37,7 @@ class CGDestimator():
         return fea_new
 
     def sampler(self,gap):
-        inds = torch.randint(0,self.gen_n,[gap.shape[0]])
+        inds = torch.randint(0, self.gen_n, [10*gap.shape[0]])
         out = self.gen_fea[inds]
         return out
 
