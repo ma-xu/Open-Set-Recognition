b4b32c9b63c3fbd4d1cfc8fed0c10efac1d932a2
13952522076 <xuma@my.unt.edu>
2020-11-14 11:05:33 -0600

    test

--- OSR/OpenMax/imagenet_val.py
+++ OSR/OpenMax/imagenet_val.py
@@ -299,6 +299,7 @@ def validate(val_loader, train_loader, model):
         with torch.no_grad():
             _, output = model(input_var)
 
+        print(f"output shape is : {output.shape}, max target is {max(target_var)}")
         scores.append(output)
         labels.append(target)
 
