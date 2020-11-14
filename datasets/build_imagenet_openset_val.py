1945bdffcd9647cca4ee5f31457e70f0325ba69a
13952522076 <xuma@my.unt.edu>
2020-11-14 12:41:53 -0600

    update

--- datasets/build_imagenet_openset_val.py
+++ datasets/build_imagenet_openset_val.py
@@ -6,6 +6,9 @@ old_path = "/home/g1007540910/DATA/"
 old_folder = "ImageNet2012"
 new_folder = "ImageNet2012_O"
 known_class_num = 500    #500
+unnknown_class_000 = 0
+unnknown_class_100 = 100
+unnknown_class_200 = 200 #  300
 unnknown_class_300 = 300 #  300
 unnknown_class_400 = 400 #  300
 unnknown_class_500 = 400 #  300
@@ -21,6 +24,80 @@ rest_folders = list(set(class_folders).difference(set(selected_known_folders)))
 selected_unknown_folders_300 = random.sample(rest_folders,unnknown_class_300)
 selected_unknown_folders_400 = random.sample(rest_folders,unnknown_class_400)
 selected_unknown_folders_500 = random.sample(rest_folders,unnknown_class_500)
+selected_unknown_folders_200 = random.sample(rest_folders,unnknown_class_200)
+selected_unknown_folders_100 = random.sample(rest_folders,unnknown_class_100)
+
+
+
+# for test set
+print("Processing validation set 100....")
+counter = 0
+for class_folder in selected_known_folders:
+    counter += 1
+    shutil.copytree(
+        os.path.join(old_path,old_folder,'val',class_folder),
+        os.path.join(old_path,new_folder,'val100',class_folder))
+    print(f"copying Val classes (known) \t \t {counter} / {known_class_num}")
+targetdir = os.path.join(old_path,new_folder,'val100','n99999999')
+os.makedirs(targetdir)
+counter = 0
+for class_folder in selected_unknown_folders_100:
+    counter += 1
+    workdir = os.path.join(old_path,old_folder,'val',class_folder)
+    for file_ in os.listdir(workdir):
+        srcFile = os.path.join(workdir, file_)
+        targetFile = os.path.join(targetdir, file_)
+        shutil.copyfile(srcFile, targetFile)
+    print(f"copying Val classes (unknown) \t \t {counter} / {unnknown_class_100}")
+
+print("done!!!")
+
+# for test set
+print("Processing validation set 200....")
+counter = 0
+for class_folder in selected_known_folders:
+    counter += 1
+    shutil.copytree(
+        os.path.join(old_path,old_folder,'val',class_folder),
+        os.path.join(old_path,new_folder,'val200',class_folder))
+    print(f"copying Val classes (known) \t \t {counter} / {known_class_num}")
+targetdir = os.path.join(old_path,new_folder,'val200','n99999999')
+os.makedirs(targetdir)
+counter = 0
+for class_folder in selected_unknown_folders_200:
+    counter += 1
+    workdir = os.path.join(old_path,old_folder,'val',class_folder)
+    for file_ in os.listdir(workdir):
+        srcFile = os.path.join(workdir, file_)
+        targetFile = os.path.join(targetdir, file_)
+        shutil.copyfile(srcFile, targetFile)
+    print(f"copying Val classes (unknown) \t \t {counter} / {unnknown_class_200}")
+
+print("done!!!")
+
+
+# for test set
+print("Processing validation set 000....")
+counter = 0
+for class_folder in selected_known_folders:
+    counter += 1
+    shutil.copytree(
+        os.path.join(old_path,old_folder,'val',class_folder),
+        os.path.join(old_path,new_folder,'val000',class_folder))
+    print(f"copying Val classes (known) \t \t {counter} / {known_class_num}")
+print("done!!!")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # # for train set
@@ -34,7 +111,7 @@ selected_unknown_folders_500 = random.sample(rest_folders,unnknown_class_500)
 #     print(f"copying Train classes \t \t {counter} / {known_class_num}")
 #
 
-
+"""
 # for test set
 print("Processing validation set 300....")
 counter = 0
@@ -108,3 +185,4 @@ for class_folder in selected_unknown_folders_500:
     print(f"copying Val classes (unknown) \t \t {counter} / {unnknown_class_500}")
 
 print("done!!!")
+"""
