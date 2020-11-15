b5904702431569bd32b86bf89a8d7de77a64a446
13952522076 <xuma@my.unt.edu>
2020-11-15 14:57:40 -0600

    update

--- OSR/DFP_end3/cifar100.py
+++ OSR/DFP_end3/cifar100.py
@@ -417,7 +417,7 @@ def detail_evalate(sim_list,dis_list,target_list, threshold):
         sim, dis, target = sim_list[i], dis_list[i], target_list[i]
         sim_value, sim_ind = sim.max(0)
         dis_value, dis_ind = dis.min(0)
-        if sim_value < args.sim_threshold and dis_value <threshold[dis_ind]:
+        if sim_value < args.sim_threshold or dis_value >1.1*threshold[dis_ind]:
         # if sim_value < args.sim_threshold:
             predict = c
         else:
