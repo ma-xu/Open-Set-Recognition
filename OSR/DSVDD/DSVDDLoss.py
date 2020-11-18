155b17c3e08a51be4478ad867721b5e2373d4e53
13952522076 <xuma@my.unt.edu>
2020-11-17 21:12:10 -0600

    update

--- OSR/DSVDD/DSVDDLoss.py
+++ OSR/DSVDD/DSVDDLoss.py
@@ -16,6 +16,6 @@ class DSVDDLoss(nn.Module):
         # loss = radius ** 2 + (1 / self.nu) * (distance**2).mean()
 
         # loss for one-class
-        loss = 10*(distance**2).mean()
+        loss = 20*(distance**2).mean()
 
         return loss
