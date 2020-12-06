45f8ad19822e6cc4b6b0a07848ee792bb8b7e214
13952522076 <xuma@my.unt.edu>
2020-12-05 20:11:45 -0600

    update

--- DFRep/V1/DFPLoss.py
+++ DFRep/V1/DFPLoss.py
@@ -15,8 +15,8 @@ class DFPLoss(nn.Module):
         sim_classification = sim_classification / self.temperature
         loss_classification = self.ce(sim_classification, targets)
 
-        distance_cosine_fea2cen = net_out["cosine_fea2cen"]
-        dist_fea2cen = 0.5 * (distance_cosine_fea2cen ** 2)  # 0.5*||d||^2
+        dist_fea2cen = net_out["cosine_fea2cen"]
+        # dist_fea2cen = 0.5 * (dist_fea2cen ** 2)  # 0.5*||d||^2
         batch_size, num_classes = dist_fea2cen.shape
         classes = torch.arange(num_classes, device=targets.device).long()
         labels = targets.unsqueeze(1).expand(batch_size, num_classes)
