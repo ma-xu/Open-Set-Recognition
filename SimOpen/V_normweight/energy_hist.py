4336550b74eb2888ab8d7f891de37a2eaccf4aa8
13952522076 <xuma@my.unt.edu>
2020-12-16 22:21:53 -0600

    update

--- SimOpen/V_normweight/energy_hist.py
+++ SimOpen/V_normweight/energy_hist.py
@@ -1,4 +1,9 @@
 import torch
+import os.path
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 def energy_hist(Out_list: torch.Tensor, Target_list:torch.Tensor, args, name:str):
@@ -13,4 +18,23 @@ def energy_hist(Out_list: torch.Tensor, Target_list:torch.Tensor, args, name:str
         unknown_hist = unknown_hist/(unknown_hist.sum())
         known_hist = known_hist/(known_hist.sum())
         name += "_normed"
+    if args.hist_save:
+        plot_bar(unknown_hist, known_hist, args, name)
     print(f"{name} processed.")
+
+
+def plot_bar(unknown_hist, known_hist, args, name):
+    x = np.arange(args.hist_bins)
+    unknown_hist = unknown_hist.data.cpu().numpy()
+    known_hist = known_hist.data.cpu().numpy()
+
+    total_width, n = 0.8, 2
+    width = total_width / n
+    x = x - (total_width - width) / 2
+
+    plt.bar(x, unknown_hist, width=width, label='unknown')
+    plt.bar(x + width, known_hist, width=width, label='known')
+    plt.legend()
+    save_name = os.path.join(args.histfolder, name + '.png')
+    plt.savefig(save_name, bbox_inches='tight', dpi=args.plot_quality)
+    plt.close()
