78b88f208cd4a91dc90677fb3b461ebd1cb0b680
13952522076 <xuma@my.unt.edu>
2021-01-19 00:09:40 -0500

    update

--- SimOpen/V_4/energy_hist.py
+++ SimOpen/V_4/energy_hist.py
@@ -10,10 +10,10 @@ def energy_hist(Out_list: torch.Tensor, Target_list:torch.Tensor, args, name:str
     unknown_label = Target_list.max()
     unknown_list = Out_list[Target_list == unknown_label]
     known_list = Out_list[Target_list != unknown_label]
-    unknown_hist = torch.histc(unknown_list, bins=args.hist_bins, min=Out_list.min().data,
-                               max=Out_list.max().data)
-    known_hist = torch.histc(known_list, bins=args.hist_bins, min=Out_list.min().data,
-                           max=Out_list.max().data)
+    unknown_hist = torch.histc(unknown_list, bins=args.hist_bins, min=Out_list.min(),
+                               max=Out_list.max())
+    known_hist = torch.histc(known_list, bins=args.hist_bins, min=Out_list.min(),
+                           max=Out_list.max())
     if args.hist_norm:
         unknown_hist = unknown_hist/(unknown_hist.sum())
         known_hist = known_hist/(known_hist.sum())
