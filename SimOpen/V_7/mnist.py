4e960d66569c7aeaf0d9d6f756e96a16e82a7dbd
13952522076 <xuma@my.unt.edu>
2021-01-27 23:44:34 -0500

    update

--- SimOpen/V_7/mnist.py
+++ SimOpen/V_7/mnist.py
@@ -121,7 +121,7 @@ def main():
     print(device)
     stage1_dict = main_stage1()  # {"net": net, "mid_energy": {"mid_known":, "mid_unkown":}}}
     middle_dict = middle_validate(stage1_dict["net"], trainloader, device, stage="1")
-    mid_energy = {"mid_known":middle_dict["middle_dict"], "mid_unkown": middle_dict["mid_unkown"]}
+    mid_energy = {"mid_known":middle_dict["mid_known"], "mid_unkown": middle_dict["mid_unkown"]}
     main_stage2(stage1_dict["net"],middle_dict["vae"], mid_energy)
 
 
