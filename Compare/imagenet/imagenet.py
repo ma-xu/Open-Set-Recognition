fde614b9de653b4a112999e45d79b1e86dc0f554
13952522076 <xuma@my.unt.edu>
2021-03-10 03:50:16 -0500

    update

--- Compare/imagenet/imagenet.py
+++ Compare/imagenet/imagenet.py
@@ -503,12 +503,12 @@ def reduce_tensor(tensor):
     return rt
 
 if __name__ == '__main__':
-    # try:
-    #     main()
-    # except Exception as e:
-    #     print(e)
-    #     traceback.print_exc()
-    #     os.system("sudo poweroff")
-    # print("DONE, FINISHED!!!")
-    # os.system("sudo poweroff")
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        os.system("sudo poweroff")
+    print("DONE, FINISHED!!!")
+    os.system("sudo poweroff")
+    # main()
