0bcea14f98132cb767069aeb651dff50c08b7f5a
13952522076 <xuma@my.unt.edu>
2021-03-11 03:27:50 -0500

    update

--- Compare/imagenet/imagenet_val.py
+++ Compare/imagenet/imagenet_val.py
@@ -370,6 +370,9 @@ def validate(val_loader, model,intervals=20):
             f"Best Possibility thre is: {best_possibility_thr} | Norm thr is :{best_norm_thr} | Energy thr is: {best_energy_thr}")
         print(f"Max predict is {Predict_list.max()}")
         print(f"Max target  is {Target_list.max()}")
+    if args.local_rank == 1:
+        print(f"Max predict is {Predict_list.max()}")
+        print(f"Max target  is {Target_list.max()}")
     return {
         "best_F1_possibility": best_F1_possibility,
         "best_F1_norm": best_F1_norm,
