b6ce7b6d1a7f9a21cc62a6ddb1703dacf6b071fa
13952522076 <xuma@my.unt.edu>
2021-03-17 21:45:49 -0400

    update

---
+++ OSR/ARPL/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+python3 osr.py --thres 0.1 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.2 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.3 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.4 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.5 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.6 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.7 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.8 --dataset cifar10 --trys 1
+python3 osr.py --thres 0.9 --dataset cifar10 --trys 1
