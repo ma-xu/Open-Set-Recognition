9bf00824ec3182baed9a3cf391f10f07d98310ef
13952522076 <xuma@my.unt.edu>
2021-03-17 23:40:43 -0400

    update

--- OSR/CenterLoss/mnist_rpl.py
+++ OSR/CenterLoss/mnist_rpl.py
@@ -216,6 +216,7 @@ def test(net, testloader,criterion_rpl, device):
     scores = scores.softmax(dim=1)
     scores = scores.cpu().numpy()
     logits = torch.cat(logits, dim=0)
+    logits = logits.cpu.numpy()
     print(f"logits.shape: {logits.shape}")
     scores = logits
     labels = torch.cat(labels, dim=0).cpu().numpy()
